7f49f1e977add92e12f784e2246b2cdb27adda13
Santhosh752 <darshansanthu@gmail.com>
2021-03-15 07:48:02 +0530

    LoginLogoutChrome

---
+++ Web-Automation/src/BasicAssignments/Assignment4.java
@@ -0,0 +1,116 @@
+package BasicAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment4 {
+	public static WebDriver oBrowser=null;
+
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		login();
+		Minimize();
+		CreateCustomer();
+		ModifyCustomer();
+		DeleteCustomer();
+
+	}
+	public static void launchBrowser()
+	{
+		try
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\driver\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost/login.do");
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void Minimize()
+	{
+		try 
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void CreateCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr/td[3]/a/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[1]/div[2]/div/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[1]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'customerLightBox_nameField\']")).sendKeys("Customer 1");
+			oBrowser.findElement(By.xpath("//*[@id=\'customerLightBox_commitBtn\']/div/span")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void ModifyCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='cpTreeBlock']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id='taskListBlock']/div[2]/div[2]/div[1]/div[3]/div[2]/div/div[1]/textarea")).sendKeys("Hello World");
+			Thread.sleep(2000);
+			System.out.println("Modified customer sucessfully");
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[4]/div/div/div[2]")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void DeleteCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[4]/div/div")).click();
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[4]/div/div[3]/div")).click();
+			oBrowser.findElement(By.xpath("//*[@id=\'customerPanel_deleteConfirm_submitTitle\']")).click();
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
